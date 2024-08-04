fb1e789d8c0fabdadb6b55403b7ebf7a86203cb8
JAKavindi <rmjakavindi@gmail.com>
2024-08-04 22:52:33 +0530

    Update Bubble sort.java

--- Bubble Sort/Bubble sort.java
+++ Bubble Sort/Bubble sort.java
@@ -1,5 +1,30 @@
-// Driver Class 
-class SelectionSort { 
-void sort(int arr[]) 
-{ 
-int n = arr.length;
+import java.util.Arrays;
+
+class Main{
+  //perform the bubble sort 
+  static void bubbleSort(int array[]){
+    int size = array.length;
+    //loop to access each array element
+    for(int i=0; i<(size-1); i++)
+      //loop to compare array elements
+      for(int j=0; j<(size-i-1); j++)
+        //compare two adjacent elements 
+        //change > to < to sort in descending order
+        if(array[j] > array[j+1]){
+          int temp = array[j];
+          array[j] = array[j+1];
+          array[j+1] = temp;
+        }
+  }
+
+  public static void main(String args[]){
+    int[] data = {-2, 45, 0, 11, -9};
+    //call method using class name
+    Main.bubbleSort(data);
+    System.out.println("Sorted Array in Ascending Order: ");
+    System.out.println(Arrays.toString(data));
+  }
+}
+    
+          
+        
