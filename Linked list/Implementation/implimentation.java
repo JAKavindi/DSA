709a21b42ee40be626fff816c26d43e8e5e2be7f
JAKavindi <rmjakavindi@gmail.com>
2024-08-31 17:33:32 +0530

    Update implimentation.java

--- Linked list/Implementation/implimentation.java
+++ Linked list/Implementation/implimentation.java
@@ -1,18 +1,37 @@
-import java.io. *;
-// Java program to implement
-// a Singly Linked List
-public class LinkedList {
-Node head; // head of list
-// Linked list Node.
-// This inner class is made static
-// so that main() can access it
+class LinkedList {
+// create an object of Node class
+// represent the head of the linked list
+Node head;
+// static inner class
 static class Node {
-int data;
+int value;
+// connect each node to next node
 Node next;
-// Constructor
-Node(int d)
-{
-data = d;
+Node(int d) {
+value = d;
 next = null;
+}
+}
+
+public static void main(String[] args) {
+// create an object of LinkedList
+LinkedList linkedList = new 
+LinkedList();
+// assign values to each linked list node
+linkedList.head = new Node(1);
+Node second = new Node(2);
+Node third = new Node(3);
+// connect each node of linked list to 
+next node
+linkedList.head.next = second;
+second.next = third;
+// printing node-value
+System.out.print("LinkedList: ");
+while (linkedList.head != null) {
+System.out.print(linkedList.head.value
++ " ");
+linkedList.head = 
+linkedList.head.next;
+}
 }
 }
