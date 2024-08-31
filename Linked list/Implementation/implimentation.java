70bdea0cbbea027bd1e482ff9f624c7c6f1a9c88
JAKavindi <rmjakavindi@gmail.com>
2024-08-31 17:28:52 +0530

    Create implimentation.java

---
+++ Linked list/Implementation/implimentation.java
@@ -0,0 +1,18 @@
+import java.io. *;
+// Java program to implement
+// a Singly Linked List
+public class LinkedList {
+Node head; // head of list
+// Linked list Node.
+// This inner class is made static
+// so that main() can access it
+static class Node {
+int data;
+Node next;
+// Constructor
+Node(int d)
+{
+data = d;
+next = null;
+}
+}
